977803867321a1d0fb1f40f649a76327ccd467c5
jacobarchambault <jacobarchambault@toolbox>
2021-04-15 07:38:56 -0400

    Rename isMember to isMemberRecursive

--- Chapter15Lab1/src/App.java
+++ Chapter15Lab1/src/App.java
@@ -12,15 +12,15 @@ public class App {
 	 * @return If the value is found, the method returns true. Otherwise, it returns
 	 *         false.
 	 */
-	public static boolean isMember(final int[] array, final int value, final int size) {
-		return (size == 0) ? false : (array[size - 1] == value) ? true : App.isMember(array, value, size - 1);
+	public static boolean isMemberRecursive(final int[] array, final int value, final int size) {
+		return (size == 0) ? false : (array[size - 1] == value) ? true : App.isMemberRecursive(array, value, size - 1);
 	}
 
 	public static void main(final String[] args) {
 		final int[] numbers = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
 		// Test all of the values from 0 through 20 and see if// they are in the array.
 		for (var x = 0; x <= 20; x++) {
-			if (App.isMember(numbers, x, numbers.length)) {
+			if (App.isMemberRecursive(numbers, x, numbers.length)) {
 				System.out.println(x + " is found in the array.\n");
 			} else {
 				System.out.println(x + " is not found in the array.\n");
